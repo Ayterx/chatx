64143d771e820e36b85ff9047962f567a57e5370
Mohammed Saleh <79310652+Ayterx@users.noreply.github.com>
2025-06-13 08:00:56 +0300

    update design

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,8 +6,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <HydrationWorkaround>
       <Sidebar />
 
-      <div className="h-svh flex-1 max-lg:pt-2 lg:p-2 lg:ps-0">
-        <main className="flex h-full flex-col rounded-2xl border-neutral-800 px-2 lg:border lg:bg-neutral-950 lg:p-2">
+      <div className="h-svh flex-1 max-lg:pt-2 lg:ps-0 lg:pt-2">
+        <main className="flex h-full flex-col rounded-ss-2xl border-neutral-800 px-2 lg:border-t lg:border-l lg:bg-neutral-950 lg:p-2">
           {children}
         </main>
       </div>
